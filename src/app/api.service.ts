514116c225e27d24964bf9fd8b2fac6b7881f439
mohommedaly <mohommedalikhan@gmail.com>
2025-05-16 15:41:45 +0530

    admin update

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ApiService {
 
-  private baseUrl = 'http://https://json-server-db-exam.onrender.com:3000';
+  private baseUrl = 'http://https://json-server-db-exam.onrender.com';
 
   private studentName: string = '';
   private questions: any[] = [];
